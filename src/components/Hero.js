3284c662e57967192dcc705eedeab0ae34dc0ae5
Velayuthamg1005 <velayuthamg1005@gmail.com>
2024-12-31 14:06:26 +0530

    Fix line endings

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,17 +10,23 @@ export default function Hero() {
         }
     }
 
-
-    return <section className='flex flex-col md:flex-row px-5 py-40 bg-primary justify-center'>
-       <div className='md:w-1/2 flex flex-col'>
-            <h1 className=' text-white text-6xl font-hero-font py-10'>Hii,This is <br/> <span className='text-black'>G</span> Velayutham
-                <p className='text-2xl py-5'>{config.subtitle}</p>
-            </h1>
-            <div className='flex py-2'>
-                <a href={config.social.Github} className='pr-5 hover:text-white'><AiOutlineGithub size={40} /></a>
-                <a href={config.social.linkedin} className='hover:text-white'><AiOutlineLinkedin size={40}/></a>
+    return (
+        <section className='flex flex-col md:flex-row px-5 py-40 bg-primary justify-center'>
+            <div className='md:w-1/2 flex flex-col'>
+                <h1 className=' text-white text-6xl font-hero-font py-10'>
+                    Hii, This is <br /> <span className='text-black'>G</span> Velayutham
+                    <p className='text-2xl py-5'>{config.subtitle}</p>
+                </h1>
+                <div className='flex py-2'>
+                    <a href={config.social.Github} className='pr-5 hover:text-white'>
+                        <AiOutlineGithub size={40} />
+                    </a>
+                    <a href={config.social.linkedin} className='hover:text-white'>
+                        <AiOutlineLinkedin size={40} />
+                    </a>
+                </div>
             </div>
-       </div>
-       <img className='md:w-1/3' src={HeroImg} />
-    </section>
+            <img className='md:w-1/3' src={HeroImg} alt="G Velayutham - Web Developer" /> {/* Added alt attribute */}
+        </section>
+    );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,7 +5,7 @@ import websiteImg3 from '../assets/readercircle.png';
 export default function Projects() {
 
     const config = {
-        projects : [
+        projects: [
             {
                 image: websiteImg1,
                 description: 'MERN Stack Todo App.',
@@ -24,29 +24,29 @@ export default function Projects() {
         ]
     }
 
-    return <section id='projects' className="flex  flex-col px-5 py-20 justify-center bg-primary text-white">
-        <div className="w-full">
-            <div className="flex flex-col px-10 py-5">
-                <h1 className="text-4xl border-b-4 border-secondary mb-5 w-[150px] font-bold">Projects</h1>
-                <p>These are some of my best projects. I have built these with React, MERN. Check them out.</p>
+    return (
+        <section id='projects' className="flex flex-col px-5 py-20 justify-center bg-primary text-white">
+            <div className="w-full">
+                <div className="flex flex-col px-10 py-5">
+                    <h1 className="text-4xl border-b-4 border-secondary mb-5 w-[150px] font-bold">Projects</h1>
+                    <p>These are some of my best projects. I have built these with React, MERN. Check them out.</p>
+                </div>
             </div>
-        </div>
-        <div className="w-full">
-            <div className='flex flex-col md:flex-row px-10 gap-5'>
-                {config.projects.map((project) => (
-                     <div className='relative'>
-                        <img className='h-[200px] w-[500px]' src={project.image}/>
-                        <div className='project-desc'>
-                            <p className='text-center px-5 py-5'>{project.description}</p>
-                            <div className='flex justify-center'>
-                                <a className='btn' target='_blank' href={project.link}>View Project</a>
+            <div className="w-full">
+                <div className='flex flex-col md:flex-row px-10 gap-5'>
+                    {config.projects.map((project, index) => (
+                        <div key={index} className='relative'>
+                            <img className='h-[200px] w-[500px]' src={project.image} alt={`Project: ${project.description}`} /> {/* Added alt attribute */}
+                            <div className='project-desc'>
+                                <p className='text-center px-5 py-5'>{project.description}</p>
+                                <div className='flex justify-center'>
+                                    <a className='btn' target='_blank' rel='noopener noreferrer' href={project.link}>View Project</a> {/* Added rel="noopener noreferrer" */}
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
-               
-               
+                    ))}
+                </div>
             </div>
-        </div>
-    </section>
+        </section>
+    );
 }
